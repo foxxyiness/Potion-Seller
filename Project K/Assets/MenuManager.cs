ec193bd7db87842c99a98c22b0ddb0d5ba9263d1
foxxyiness <78097234+foxxyiness@users.noreply.github.com>
2023-09-08 16:21:23 -0400

    Build 1
    You can run it and quit the game

---
+++ Project K/Assets/MenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QU/IT");
+    }
+}
