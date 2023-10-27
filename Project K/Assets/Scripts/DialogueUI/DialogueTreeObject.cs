77f154924b114bc73922ebef68dbf0b7efd76127
foxxyiness <78097234+foxxyiness@users.noreply.github.com>
2023-10-27 11:57:38 -0400

    Revert "revert"
    This reverts commit ffb5773ae916a849f07dd73aec2ad17acfd9538e.

---
+++ Project K/Assets/Scripts/DialogueUI/DialogueTreeObject.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Dialogue
+{
+    [CreateAssetMenu(fileName = "DialogueTree", menuName = "ScriptableObjects/Dialogue Tree")]
+
+    public class DialogueTreeObject : ScriptableObject
+    {
+        public string npcName;
+        public string defaultState;
+        public DialogueOption defaultOption;
+        public string[] scriptableCallbackNames;
+        public DialogueUnit[] dialogueUnits;
+
+
+        //non editor class members
+        public DialogueState dialogueState;
+        public Action continueCallback;
+        public Action endDialogueCallback;
+        public Dictionary<string, DialogueUnit> dialogueUnitsDict;
+        public Dictionary<string, Action> scriptableCallbacks = new Dictionary<string, Action>();
+
+        public void AddToState(string stateToAdd)
+        {
+            dialogueState.stateDict[npcName] += stateToAdd;
+
+        }
+        public void ResetState(int length = 1) 
+        {
+            if (dialogueState.stateDict[npcName].Length < length)
+                return;
+            dialogueState.stateDict[npcName] = dialogueState.stateDict[npcName].Remove(dialogueState.stateDict[npcName].Length-length);
+
+        }
+
+       public void CallScriptableAction(string actionName)
+        {
+            scriptableCallbacks[actionName]();
+        }
+        
+        public void Continue()
+        {
+            continueCallback();
+        }
+
+        public void EndDialogue() 
+        {
+            endDialogueCallback();
+        
+        }
+
+        public void RegisterScriptableCallback(string callbackName, Action action)
+        {
+            scriptableCallbacks[callbackName] = action;
+        }
+
+
+        public void SetUpDialogueUnitsDict()
+        {
+            dialogueUnitsDict = new Dictionary<string, DialogueUnit>();
+            foreach (var dialogueUnit in dialogueUnits)
+            {
+                dialogueUnitsDict[dialogueUnit.requiredStateKey] = dialogueUnit;
+            }
+
+        }
+        public void SetUpDialogueState(DialogueState state)
+        {
+            dialogueState = state;
+            if (!dialogueState.stateDict.ContainsKey(npcName))
+            {
+                dialogueState.stateDict[npcName] = defaultState;
+            }
+        }
+
+        public void ResetCallbacks()
+        {
+            continueCallback = () => { };
+            endDialogueCallback = () => { };
+            scriptableCallbacks.Clear();
+        }
+
+        public DialogueUnit GetNextDialogueUnit()
+        {
+            return dialogueUnitsDict.TryGetValue(dialogueState.stateDict[npcName], out var value) ? value : null;
+        }
+
+
+
+   
+    }
+}
---
+++ Project K/Assets/Scripts/DialogueUI/Interactor.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Interactor : MonoBehaviour
+{
+
+    [SerializeField] private float interactRadius;
+    [SerializeField] private InputAction inputAction;
+    private Collider[] _collidersInRange;
+    private List<Interactable> _interactablesInRange;
+    private Interactable _closestInteractable;
+
+    private void OnEnable()
+    {
+        inputAction.Enable();
+    }
+    private void OnDisable()
+    {
+        inputAction.Disable();
+    }
+
+
+
+    private void Start()
+    {
+        _interactablesInRange = new List<Interactable>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputAction.IsPressed())
+        {
+            Debug.Log("Interacting");
+            Interact();
+        }
+    }
+
+    private void Interact()
+    {
+        if (_closestInteractable != null)
+        {
+            _closestInteractable.Interact();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_closestInteractable != null)
+        {
+            UpdateInteractables();
+        }
+    }
+    private void OnTriggerExit(Collider other) 
+    {  
+        if (_closestInteractable != null)
+        {
+            UpdateInteractables();
+        } 
+    }
+    private void UpdateInteractables()
+    {
+        _collidersInRange = Physics.OverlapSphere(transform.position, interactRadius);
+        _interactablesInRange.Clear();
+        if (_collidersInRange == null || _collidersInRange.Length == 0)
+            return;
+        foreach (var colliderInRange in _collidersInRange)
+        {
+            var interactable = colliderInRange.GetComponentInParent<Interactable>();
+            if (interactable != null)
+            {
+                _interactablesInRange.Add(interactable);
+            }
+        }
+        _closestInteractable = GetClosestInteractable();
+    }
+    private Interactable GetClosestInteractable()
+    {
+        if (_interactablesInRange.Count <= 0) return null;
+
+        int closestInteractableIndex = 0;
+        var closestDistance = Mathf.Infinity;
+        for(int i = 0; i < _interactablesInRange.Count; i++)
+        {
+            var distance = Vector3.Distance(_interactablesInRange[i].transform.position, transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestInteractableIndex = i;
+            }
+
+        }
+        return _interactablesInRange[closestInteractableIndex];
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
