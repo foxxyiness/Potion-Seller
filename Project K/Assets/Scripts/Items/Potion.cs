7d664a4b43095ecccdc5ecae58e1b2ca870c4800
foxxyiness <78097234+foxxyiness@users.noreply.github.com>
2024-01-19 16:35:42 -0500

    Potion Archetype Addition
    introduced potion archetype

--- Project K/Assets/Scripts/Items/Potion.cs
+++ Project K/Assets/Scripts/Items/Potion.cs
@@ -6,9 +6,11 @@ public class Potion : MonoBehaviour
     [SerializeField] private GameObject toastedUpgrade;
     [SerializeField] private bool toastable;
     [SerializeField] private int roastLevel;
+    [SerializeField] private string archetype;
     [SerializeField] private string baseText;
     [SerializeField] private string strengthText;
     [SerializeField] private string flavorText;
+    [SerializeField] private Archetype archeType;
     [SerializeField] private Base baseType;
     [SerializeField] private Strength strengthType;
     [SerializeField] private Flavor flavorType;
@@ -18,24 +20,46 @@ public class Potion : MonoBehaviour
         roastLevel = 2;
     }
 
+    private enum Archetype
+    {
+        Light,
+        Darkness,
+        Harmony,
+        Chaos,
+        Coffee
+    }
     private enum Base
     {
         Water,
         Light,
-        Darkness
+        Darkness,
+        Harmony,
+        Chaos,
+        Coffee
         
     }
     private enum Strength
     {
         Sun,
         StormVine,
+        PheonixBerry,
+        AshCapMushroom,
+        GoblinLeaf,
         Fireshot
     }
     private enum Flavor
     {
         Hair,
         IcePepper,
+        BarbedBamboo,
+        PixieCabbage,
+        EagleBlossom,
         Toasted
+    }
+
+    public string GetArchetype()
+    {
+        return archetype.ToString();
     }
     public string GetToastable()
     {
