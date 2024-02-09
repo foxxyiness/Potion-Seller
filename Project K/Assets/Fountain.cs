606ebabca6c6b629f6db205b964975f96f8e76aa
foxxyiness <coachfoxjp@gmail.com>
2024-02-09 14:02:53 -0500

    Fountain Spawn Water

---
+++ Project K/Assets/Fountain.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AdaptivePerformance.VisualScripting;
+
+public class Fountain : MonoBehaviour
+{
+    [SerializeField] private GameObject waterOrb;
+    [SerializeField] private float waterSpawnRate;
+    private bool _canSpawnWater;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Fire") && _canSpawnWater)
+        {
+            _canSpawnWater = false;
+            StartCoroutine(SpawnWater());
+        }
+    }
+
+    private IEnumerator SpawnWater()
+    {
+        Instantiate(waterOrb, transform.position, quaternion.identity);
+        yield return new WaitForSeconds(waterSpawnRate);
+        _canSpawnWater = true;
+    }
+}
