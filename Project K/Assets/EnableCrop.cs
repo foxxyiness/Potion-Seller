e841f1ed087c9577208478f36d41715f57b84dc9
foxxyiness <78097234+foxxyiness@users.noreply.github.com>
2024-03-29 00:34:28 -0400

    Updates for Enable Crop Requires more Testing

--- Project K/Assets/EnableCrop.cs
+++ Project K/Assets/EnableCrop.cs
@@ -7,36 +7,64 @@ public class EnableCrop : MonoBehaviour
 {
     [SerializeField] private GameObject leftHand;
     [SerializeField] private List<GrowthStage> cropGrowthStage;
+    [SerializeField] private GameObject player;
     private bool _enableCrop;
+    private bool _isCropEnabled;
     private float timer = 2f;
 
     private void Start()
     {
         _enableCrop = false;
+        cropGrowthStage.Add(gameObject.GetComponentInChildren<GrowthStage>());
     }
 
     private void Update()
     {
          StartCrop();
+         CheckPlayer();
     }
-
+    
+    //If enable crop is true, checks transform of left hand if true, starts 2 second timer, then enable crops
     private void StartCrop()
     {
-        if (_enableCrop)
+        if (!_enableCrop) return;
+        if (leftHand.transform.rotation.x > 65f)
         {
-            if (leftHand.transform.rotation.x > 65f)
+            timer += Time.deltaTime;
+            if (timer >= 2)
             {
-                timer += Time.deltaTime;
-                if (timer >= 2)
+                IsCropEnabled();
+                foreach (var crop in cropGrowthStage)
                 {
-                    _enableCrop = false;
-                    foreach (var crop in cropGrowthStage)
-                    {
-                        crop.EnableCrop();
-                    }
+                    crop.EnableCrop();
                 }
             }
         }
+        else
+        {
+            timer = 0;
+        }
+    }
+
+   private void CheckPlayer()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) <= 1)
+        {
+            _enableCrop = true;
+            Debug.Log("Player in position of " + gameObject.name);
+        }
+        else
+        {
+            _enableCrop = false;
+        }
+    } 
+    
+    private void IsCropEnabled()
+    {
+        if (_isCropEnabled)
+            _isCropEnabled = false;
+        else if (!_isCropEnabled)
+            _isCropEnabled = true;
     }
     private void OnCollisionEnter(Collision other)
     {
@@ -45,7 +73,7 @@ public class EnableCrop : MonoBehaviour
             _enableCrop = true;
         }
     }
-
+    
     private void OnCollisionExit(Collision other)
     {
         if (other.collider.CompareTag("Player"))
