4032753359c72ccb0c535feedf1bd9c466646970
foxxyiness <78097234+foxxyiness@users.noreply.github.com>
2024-04-06 23:35:19 -0400

    Added Text for Tutorial

--- Project K/Assets/TutorialManager.cs
+++ Project K/Assets/TutorialManager.cs
@@ -7,6 +7,7 @@ using TMPro;
 using UI___Menu;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit;
 using Random = System.Random;
@@ -19,6 +20,13 @@ public class TutorialManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI textContent;
     [SerializeField] private GameObject nextButton;
     [SerializeField] private GameObject orderUIContent;
+    
+    [Header("Tutorial Instantiation Objects")] 
+    [SerializeField] private GameObject cauldron;
+    [SerializeField] private GameObject icepepperCrop;
+    [SerializeField] private GameObject stormvineCrop;
+    
+    
     TextMeshProUGUI text; 
     void Start()
     {
@@ -85,7 +93,7 @@ public class TutorialManager : MonoBehaviour
             case 3:
             {
                 contentString =
-                    "You've been empowered by your godly powers, lets try your fire ability.\n Lift your right hand, " +
+                    "You've been born with godly abilities, meaning you are a god. Now, lets try your fire ability.\n Lift your right hand, " +
                     "hold the grip button, and tap the trigger.";
                 textContent.text = contentString;
                 text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation, orderUIContent.transform);
@@ -130,22 +138,216 @@ public class TutorialManager : MonoBehaviour
                 break;
             }
 
+            case 8:
+            {
+                contentString =
+                    "Try it for yourself. Grab the water orb and put it into the Cauldron.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            
+            case 9:
+            {
+                contentString =
+                    "Great. Water orbs is the base of all potions. Every potion needs water. Now grab the sun orb and put it into the Cauldron.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+
+            case 10:
+            {
+                contentString =
+                    "Great. Sun orbs is the strength component of potions. Different strength ingredients are grown from your crops. \n " +
+                    "Now grab the hair orb and put it into the Cauldron.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            
+            case 11:
+            {
+                contentString =
+                    "You have made the Potion of Light. Light Potions are unique and can be toasted using your fire ability. \n" +
+                    "Lets try it now. Shoot the Potion of Light. ";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            
+            case 12:
+            {
+                contentString =
+                    "Excellent! Now look behind you to find your crops.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            
+            case 13:
+            {
+                contentString =
+                    "Your farm will contains the necessary ingredients to make all potions, " +
+                    "but worldly constraints prevent are preventing them from growing.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 14:
+            {
+                contentString =
+                    "Your godly aura can make the crops grow. Stand on top of the crop sign and look at your mana ring on your left hand.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 15:
+            {
+                contentString =
+                    "You mana ring on your left hand shows the status of your mana control over the crops.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 16:
+            {
+                contentString =
+                    "You sun ability is used to grow crops faster. Now try it out. Shoot one of the crops with the power of the sun.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 17:
+            {
+                contentString =
+                    "Great, this ingredient, known as Ice Pepper. Now grab it and put it into the potion.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 18:
+            {
+                contentString =
+                    "Now turn on the Storm Vine crop, use your sun ability, and put Storm Vine into the cauldron.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 19:
+            {
+                contentString =
+                    "Now add a Water Orb into the Cauldron.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 20:
+            {
+                contentString =
+                    "You have now created a Potion of Darkness. Now put it into the box to the left.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 21:
+            {
+                contentString =
+                    "This is how you will deposit the potions you make that are on order. " +
+                    "\n Now, lets try making that Potion of Darkness into Coffee";
+                //Spawn Potion of Darkness 
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 22:
+            {
+                contentString =
+                    "Great, breaking the coffee will give you a temporary speed boost.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 23:
+            {
+                contentString =
+                    "Now look towards your right for your recipe book. There you'll find your found potion recipes.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 24:
+            {
+                contentString =
+                    "If you see a potion on order that you've never seen before, you must find the lost page.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 25:
+            {
+                contentString =
+                    "Now, Shoot the lost page with your fire ability.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 26:
+            {
+                contentString =
+                    "Once shot, the lost page will added to your recipe book.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 27:
+            {
+                contentString =
+                    "Remember this bell, it rings only 3 times a day. If you complete all orders, \n " +
+                    "you will hear the single bell ring at the start of the day.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            case 28:
+            {
+                contentString =
+                    "If you hear this bell, you have failed to complete all orders, and your Earthly contract will be voided.";
+                textContent.text = contentString;
+                text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation,
+                    orderUIContent.transform);
+                break;
+            }
+            
             default:
             {
-                contentString = "Great, ready to begin. Press Play to load in.";
+                contentString = "You Earth-Bound contract begins now. Don't fail us.";
                 textContent.text = contentString;
                 text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation, orderUIContent.transform);
                 nextButton.SetActive(true);
                 break;
             }
-            // case 9:
-            // {
-            //     contentString =
-            //         "Lets try it. Look behind you and put the ingredients into the cauldron. Use your trigger button to grab an item.";
-            //     textContent.text = contentString;
-            //     text = Instantiate(textContent, orderUIContent.transform.position, orderUIContent.transform.rotation, orderUIContent.transform);
-            //     break;
-            // }
+            
 
         }
         
